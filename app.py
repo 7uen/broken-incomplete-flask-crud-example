d3473e6157e9f0474efb6031225af15746823121
dHunstein <anton7meier@gmail.com>
2022-12-08 18:23:19 +0100

    test23

--- app.py
+++ app.py
@@ -14,58 +14,68 @@ app.config['MYSQL_DB'] = 'student'
 app.config['MYSQL_HOST'] = '35.205.42.222'
 mysql.init_app(app)
 
-@app.route("/add") #Add Student
-def add():
-  name = request.args.get('name')
-  email = request.args.get('email')
-  cur = mysql.connection.cursor() #create a connection to the SQL instance
-  s='''INSERT INTO students(studentName, email) VALUES('{}','{}');'''.format(name,email) # kludge - use stored proc or params
-  cur.execute(s)
-  mysql.connection.commit()
 
-  return '{"Result":"Success"}' # Really? maybe we should check!
-  
-@app.route("/") #Default - Show Data
-def read(): # Name of the method
-  cur = mysql.connection.cursor() #create a connection to the SQL instance
-  cur.execute('''SELECT * FROM students''') # execute an SQL statment
-  rv = cur.fetchall() #Retreive all rows returend by the SQL statment
-  Results=[]
-  for row in rv: #Format the Output Results and add to return string
-    Result={}
-    Result['Name']=row[0].replace('\n',' ')
-    Result['Email']=row[1]
-    Result['ID']=row[2]
-    Results.append(Result)
-  response={'Results':Results, 'count':len(Results)}
-  ret=app.response_class(
-    response=json.dumps(response),
-    status=200,
-    mimetype='application/json'
-  )
-  return ret #Return the data in a string format
+@app.route("/add")  # Add Student
+def add():
+    name = request.args.get('name')
+    email = request.args.get('email')
+    cur = mysql.connection.cursor()  # create a connection to the SQL instance
+    s = '''INSERT INTO students(studentName, email) VALUES('{}','{}');'''.format(
+        name, email)  # kludge - use stored proc or params
+    cur.execute(s)
+    mysql.connection.commit()
+
+    return "<h1>Added user</h1>"  # Really? maybe we should check!
+
+
+@app.route("/")  # Default - Show Data
+def read():  # Name of the method
+    cur = mysql.connection.cursor()  # create a connection to the SQL instance
+    cur.execute('''SELECT * FROM students''')  # execute an SQL statment
+    rv = cur.fetchall()  # Retreive all rows returend by the SQL statment
+    #Results = []
+    html = ""
+    for row in rv:  # Format the Output Results and add to return string
+        Result = {}
+        Result['Name'] = row[0].replace('\n', ' ')
+        Result['Email'] = row[1]
+        Result['ID'] = row[2]
+        #Results.append(Result)
+        html = html + \
+            (f"<tr style='border: 1px solid blue'><th  style='border: 1px solid blue'>{Result['Name']}</th> <th style='border: 1px solid blue'>{Result['Email']}</th></tr> <br>")
+    html = f"<table style='border: 1px solid blue'><tr style='border: 1px solid blue'><th  style='border: 1px solid blue'>Name</th><th style='border: 1px solid blue'>Email</th></tr>{html}</table>"
+    return html
+    #response = {'Results': Results, 'count': len(Results)}
+    #ret = app.response_class(
+       # response=json.dumps(response),
+      #  status=200,
+    #    mimetype='application/json'
+    #)
+   # return ret  # Return the data in a string format
+
 
 @app.route("/delete")
 def delete():
-  name = request.args.get('name')
-  cur = mysql.connection.cursor()
-  str = f"DELETE from students where studentName = '{name}'"
-  print(str)
-  cur.execute(str)
-  mysql.connection.commit()
-  print("worked")
-  return "<h1>Deleted user</h1>"
+    name = request.args.get('name')
+    cur = mysql.connection.cursor()
+    str = f"DELETE from students where studentName = '{name}'"
+    print(str)
+    cur.execute(str)
+    mysql.connection.commit()
+    print("worked")
+    return "<h1>Deleted user</h1>"
+
 
 @app.route("/update")
 def update():
-  name = request.args.get('name')
-  email = request.args.get('email')
-  cur = mysql.connection.cursor()
-  cur.execute(
-      f"UPDATE students set email = '{email}' where studentName = '{name}'")
-  mysql.connection.commit()
-  return "<h1>Updated user</h1>"
+    name = request.args.get('name')
+    email = request.args.get('email')
+    cur = mysql.connection.cursor()
+    cur.execute(
+        f"UPDATE students set email = '{email}' where studentName = '{name}'")
+    mysql.connection.commit()
+    return "<h1>Updated user</h1>"
+
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',port='8080') #Run the flask app at port 8080
-
+    app.run(host='0.0.0.0', port='8080')  # Run the flask app at port 8080
